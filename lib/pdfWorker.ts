5054bace10c7316774772726463fcebc54c88678
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2025-03-30 18:52:28 +0200

    A

--- lib/pdfWorker.ts
+++
@@ -1,5 +0,0 @@
-// lib/pdfWorker.ts
-import { GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
-import workerSrc from "pdfjs-dist/legacy/build/pdf.worker.min.js?url";
-
-GlobalWorkerOptions.workerSrc = workerSrc;
