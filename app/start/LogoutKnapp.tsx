334960d2b8fb8f924ce4ebc8a7f7e310608dabd3
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2025-05-31 13:51:11 +0200

    asdf

--- app/start/LogoutKnapp.tsx
+++ app/start/LogoutKnapp.tsx
@@ -12,7 +12,7 @@ function LogoutButton() {
     <div className="flex items-center justify-end text-white md:fixed md:right-0 md:top-1 md:m-4">
       <span className="hidden mr-6 md:inline md:-mt-1">{session.user?.name}</span>
       <button
-        onClick={() => signOut()}
+        onClick={() => signOut({ callbackUrl: "/" })} // ← Lägg till denna!
         className="px-4 py-2 font-bold text-white transition duration-300 bg-transparent border border-white rounded hover:bg-white hover:bg-opacity-20"
       >
         Logga ut
