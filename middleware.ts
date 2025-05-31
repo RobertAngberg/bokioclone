52dd8f8447ed01fb66dabbbe482cf607a949ff0a
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2025-05-31 14:07:08 +0200

    asdf

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 export { auth as middleware } from "@/auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|$).*)"],
-  //                                                     ↑ exkluderar root
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  // ↑ Tog bort |$ så root också kollas
 };
